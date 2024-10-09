0fa2bd4a7400d82a249262a994147710b63b8e1a
gustavormuller <gusrmuller@gmail.com>
2024-10-09 04:00:57 -0300

    feat: layout

--- example/resources/views/Components/layout.blade.php
+++ example/resources/views/Components/layout.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
+    <title>Home</title>
 </head>
+
 <body>
-    <h1>Hello from the about page.</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    {{ $slot }}
 </body>
+
 </html>
---
+++ example/resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Get in touch: +55 27 993233269</h1>
+</x-layout>
